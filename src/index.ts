baf9b5d664d618befb6b31da12251dd9e66acc01
Vlad <vlad.jerez@gmail.com>
2022-02-10 07:56:23 -0400

    progreso
    un minimi avance en la noche

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import express from "express";
-
+// Initializations
 const app = express();
+
+// Settings
+app.set('port',3000);
+// Middlewares
+
+// Routes
+
+// start server
+app.listen(app.get('port'), () => {
+    console.log("Server is running on: http://localhost:3000");
+});
