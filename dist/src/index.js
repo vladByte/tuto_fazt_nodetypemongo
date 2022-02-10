96fab019475d590ee7fb3232de86d5a3bfea4789
Vlad <vlad.jerez@gmail.com>
2022-02-10 17:28:06 -0400

    progreso del tutorial
    Se configuro handlebars, (views), los archivos staticos y el path

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,5 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const Express_1 = __importDefault(require("Express"));
-const app = (0, Express_1.default)();
+const express_1 = __importDefault(require("express"));
+// Initializations
+const app = (0, express_1.default)();
+// Settings
+app.set('port', 3000);
+// Middlewares
+// Routes
+app.get('/', (req, res) => {
+    res.send("Hola");
+});
+// start server
+app.listen(app.get('port'), () => {
+    console.log("Server is running on: http://localhost:3000");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import express from "express";
+import {engine} from "express-handlebars";
+import path from "path";
+
 // Initializations
 const app = express();
 
 // Settings
-app.set('port',3000);
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+    // handlebars settings 
+app.engine('.hbs', engine({
+    extname : '.hbs',
+    layoutsDir : path.join(app.get('views') , 'layouts'),
+    partialsDir : path.join(app.get('views') , 'partials'),
+    helpers: require('./lib/helpers')
+}));
+app.set('view engine', '.hbs');
+
 // Middlewares
+app.use(express.json()); // El servidor pueda ententer json.
 
 // Routes
+app.get('/',(req, res) => {
+    res.send("Hola Vlad");
+});
+app.get('/books', (req, res) => {res.send('Books')});
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // start server
 app.listen(app.get('port'), () => {
-    console.log("Server is running on: http://localhost:3000");
+    console.log(`Server is running on: http://localhost:${app.get('port')}`);
 });
